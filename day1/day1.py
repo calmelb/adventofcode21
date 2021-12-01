5e7746c3bc6328ec4142eb01a47a5d6d1735bef5
Callum <10206838+calmelb@users.noreply.github.com>
2021-12-02 03:28:20 +1100

    Part 1 sorted

--- day1/day1.py
+++ day1/day1.py
@@ -2,7 +2,9 @@
 
 #Variable Definitions
 previousNo = 0
+previousNoback = 0
 larger = 0
+datalist = []
 # Text File processing
 tempdata = [199,
             200,
@@ -13,17 +15,35 @@ tempdata = [199,
             240,
             269,
             260,
-            263]
-#TODO: Import text file
+            263,
+            1,
+            5555,
+            23,
+            24,
+            -5,
+            10]
+
+
+inputfile = open("input.txt", "r")
+filecontent = inputfile.readlines()
+
+for element in filecontent:
+    datalist.append(element.strip())
 
 # Logic Analysis
-for i in tempdata:
+for i in datalist:
     if previousNo != 0:
         if previousNo < i:
-            # print("Larger: "+ str(i))
-            # print("Smaller: "+ str(previousNo))
-            # print("_______________")
+                # print("Larger: "+ str(i))
+                # print("Smaller: "+ str(previousNo))
+                # print("_______________")
             larger = larger + 1
+        # else:
+        #     print("Previous Comp: "+ str(previousNoback))
+        #     print("Current No.: "+ str(i))
+        #     print("Previous No.: "+ str(previousNo))
+        #     print("_______________")
+    previousNoback = previousNo
     previousNo = i
 
 print(larger)
